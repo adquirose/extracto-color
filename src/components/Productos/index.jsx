98ef38889248228a24fb0356837f29d112847fca
Arturo Quiros <adquirose@gmail.com>
2023-02-17 21:22:01 -0300

    rutas imagenes

--- src/components/Productos/index.jsx
+++ src/components/Productos/index.jsx
@@ -6,22 +6,22 @@ const productos = [
     {
         name:'Manta de eucaliptus | tinte natural', 
         price: '$30.000 clp', 
-        image: './src/assets/img1.jpg'
+        image: 'https://cdn.shopify.com/s/files/1/0402/1990/2109/collections/8e6f72342908b72b75e4384cc11716e9_720x.jpg?v=1666524971'
     },
     {
         name:'Toalla | tinte natural', 
         price: '$20.000 clp', 
-        image: './src/assets/img1.jpg'
+        image: 'https://cdn.shopify.com/s/files/1/0402/1990/2109/collections/8e6f72342908b72b75e4384cc11716e9_720x.jpg?v=1666524971'
     },
     {
         name:'Toalla | tinte natural', 
         price: '$20.000 clp', 
-        image: './src/assets/img1.jpg'
+        image: 'https://cdn.shopify.com/s/files/1/0402/1990/2109/collections/8e6f72342908b72b75e4384cc11716e9_720x.jpg?v=1666524971'
     },
     {
         name:'Toalla | tinte natural', 
         price: '$20.000 clp', 
-        image: './src/assets/img1.jpg'
+        image: 'https://cdn.shopify.com/s/files/1/0402/1990/2109/collections/8e6f72342908b72b75e4384cc11716e9_720x.jpg?v=1666524971'
     }
 ]
 export default function Productos() {
